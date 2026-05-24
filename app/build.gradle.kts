22465c1e2070c94c1a3dc507911a2fce57569764
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-23 18:32:55 -0600

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
 }
