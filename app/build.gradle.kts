3b0377a5235c04755724545fb8d77c81e2a0053c
YenyRM <rossrecinos05@gmail.com>
2026-05-24 12:12:59 -0600

    Historial Evento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
 implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
---
+++ app/src/main/java/com/controldegastos/segundo_proyecto_kotlin/DetalleEventoActivity.kt
@@ -0,0 +1,12 @@
+package com.controldegastos.segundo_proyecto_kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalleEventoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_evento)
+    }
+}
